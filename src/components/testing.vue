a8cf0a87333d018e172a73f5f09d99b6b273a4b5
elebymax <m020203@gmail.com>
2017-02-06 19:58:24 +0800

    Modify link’s words

--- src/components/testing.vue
+++ src/components/testing.vue
@@ -1,16 +1,25 @@
 <template>
   <div id="testing">
     <h3>40人的排班花費不到<span style="color: #ffbb22;">一秒鐘</span>！</br>
-      想親自試一下嗎？這個<span><a href="https://nas.elebymax.net:5000/fbsharing/5tfc3u7w">範例檔案</a></span>給你測試真偽！</h3>
+      想親自試一下嗎？這個
+      <span><a href="https://nas.elebymax.net:5000/fbsharing/5tfc3u7w">範例檔案 [下載]</a></span>
+      給你測試真偽！
+    </h3>
   </div>
 </template>
 <script type="es6">
   export default {
     name: 'testing',
+    mounted () {
+
+    },
     data () {
       return {
-
+        animate: true
       }
+    },
+    methods: {
+
     }
   }
 </script>
@@ -20,6 +29,7 @@
     background-color: #eee;
     padding: 18px;
   }
+
   #testing h3 {
     line-height: 1.65em;
   }
