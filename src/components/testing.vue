1a3b3d15a59ffea746829a1e3e3aaf7885a20bbe
elebymax <m020203@gmail.com>
2017-02-06 12:18:03 +0800

    Modify the example file link

--- src/components/testing.vue
+++ src/components/testing.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="testing">
     <h3>40人的排班花費不到<span style="color: #ffbb22;">一秒鐘</span>！</br>
-      想親自試一下嗎？這個<span><a href="https://drive.google.com/file/d/0B93pN4EGN5z5eGZKYW9aS1llbVk/view?usp=sharing">範例檔案</a></span>給你測試真偽！</h3>
+      想親自試一下嗎？這個<span><a href="https://nas.elebymax.net:5000/fbsharing/5tfc3u7w">範例檔案</a></span>給你測試真偽！</h3>
   </div>
 </template>
 <script type="es6">
