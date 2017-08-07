7e6c29f48d817ff7286422658de0f828822565df
elebymax <m020203@gms.tku.edu.tw>
2017-08-08 00:52:31 +0800

    Finish findColsAttr and findRowsAttr methods.

--- src/assets/compute.js
+++ src/assets/compute.js
@@ -3,6 +3,7 @@
  * Created by max on 2017/1/20.
  */
 var peopleCountInShift = 0; //每班需要幾人
+const LINES_NUMBER_FROM = 1;
 
 var shifting = function (fileToRead, peopleCount) {
   peopleCountInShift = peopleCount;
@@ -31,11 +32,95 @@ var processData = function (csv) {
   formatData(lines);
 };
 
-var formatData = function (lines) {
-  
+var formatData = async function (lines) {
+  const peopleCount = lines.length-1;
+
+  let colsAttrList = await findColsAttr(lines[0]);
+  let rowsAttrList = await findRowsAttr(lines, colsAttrList);
+  console.log(rowsAttrList);
+
 };
 
+let findColsAttr = async function ( firstLine ) {
 
+  var resultList = [];
+
+  for(var i=0; i<firstLine.length; i++) {
+    var myString = firstLine[i];
+    var myRegexp = /([0-9]+)[.]{1}([^\\s]+$)/g;
+    var match = myRegexp.exec(myString);
+    if (match) {
+      var attr = {
+        position: i,
+        number: match[1],
+        name: match[2],
+      };
+      resultList.push(attr);
+    }
+  }
+
+  resultList = await sortObjectInListByNumber(resultList);
+
+  return Promise.resolve(resultList);
+
+};
+
+let findRowsAttr = async function ( lines, colsAttrList ) {
+
+  let shiftsPositionList = [];
+  for (var i=0; i<colsAttrList.length; i++) {
+    if (colsAttrList[i].position !== 0) {
+      shiftsPositionList.push(colsAttrList[i].position);
+    }
+  }
+
+  let maxShiftsNumber = 0;
+  let rowNameList = [];
+  for (var i=LINES_NUMBER_FROM; i<lines.length; i++) {
+    for (var j=0; j<shiftsPositionList.length; j++) {
+      const shiftsPosition = shiftsPositionList[j];
+      const personShiftsList = lines[i][shiftsPosition].split(";");
+      for (var k=0; k<personShiftsList.length; k++) {
+        console.log()
+        const string = personShiftsList[k];
+        const regex = /([0-9]+)[.]{1}([^\\s]+$)/g;
+        var match = regex.exec(string);
+        if (match) {
+          const number = match[1];
+          if ( maxShiftsNumber < number ) {
+            maxShiftsNumber = number;
+          }
+          if ( !checkIfNumberAlreadyInObjectList(rowNameList, number) ) {
+            const rowName = {
+              number: number,
+              name: match[2]
+            };
+            rowNameList.push(rowName);
+          }
+        }
+      }
+    }
+  }
+
+  rowNameList = await sortObjectInListByNumber(rowNameList);
+
+  return Promise.resolve(rowNameList);
+};
+
+let checkIfNumberAlreadyInObjectList = function (list, number) {
+  for (var i=0; i<list.length; i++) {
+    if (list[i].number === number) {
+      return true;
+    }
+  }
+  return false;
+};
+
+let sortObjectInListByNumber = async function (list) {
+  return Promise.resolve( list.sort(function (a, b) {
+    return a.number > b.number ? 1 : -1;
+  }) );
+};
 
 var compute = {
   shifting: shifting,
