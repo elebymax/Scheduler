6092543794c6b6874b13df00068f94410b7e11a4
elebymax <m020203@gms.tku.edu.tw>
2017-08-22 16:17:52 +0800

    FindAttrList: Make findColsAttr and findRowsAttr methods to a independent file.

--- src/assets/compute.js
+++ src/assets/compute.js
@@ -2,6 +2,8 @@
 /**
  * Created by max on 2017/1/20.
  */
+var FindAttrList = require('./findAttrList');
+
 var peopleCountInShift = 0; //每班需要幾人
 const LINES_NUMBER_FROM = 1;
 
@@ -35,74 +37,11 @@ var processData = function (csv) {
 var formatData = async function (lines) {
   const peopleCount = lines.length-1;
 
-  let colsAttrList = await findColsAttr(lines[0]);
-  let rowsAttrList = await findRowsAttr(lines, colsAttrList);
+  let colsAttrList = await FindAttrList.findColsAttr(lines[0]);
+  let rowsAttrList = await FindAttrList.findRowsAttr(LINES_NUMBER_FROM, lines, colsAttrList);
+  // let peopleInfoList = await handlePeopleInfo(lines, colsAttrList[0].position);
 
-  console.log(colsAttrList);
-};
-
-const findColsAttr = async function ( firstLine ) {
-
-  var resultList = [];
-
-  for(var i=0; i<firstLine.length; i++) {
-    var myString = firstLine[i];
-    var myRegexp = /([0-9]+)[.]{1}([^\\s]+$)/g;
-    var match = myRegexp.exec(myString);
-    if (match) {
-      var attr = {
-        position: i,
-        number: match[1],
-        name: match[2],
-      };
-      resultList.push(attr);
-    }
-  }
-
-  resultList = await sortObjectInListByNumber(resultList);
-
-  return Promise.resolve(resultList);
-
-};
-
-const findRowsAttr = async function ( lines, colsAttrList ) {
-
-  let shiftsPositionList = [];
-  for (var i=0; i<colsAttrList.length; i++) {
-    if (colsAttrList[i].position !== 0) {
-      shiftsPositionList.push(colsAttrList[i].position);
-    }
-  }
-
-  let rowNameList = [];
-  for (var i=LINES_NUMBER_FROM; i<lines.length; i++) {
-    for (var j=0; j<shiftsPositionList.length; j++) {
-      const shiftsPosition = shiftsPositionList[j];
-      const personShiftsList = lines[i][shiftsPosition].split(";");
-      for (var k=0; k<personShiftsList.length; k++) {
-        const string = personShiftsList[k];
-        const regex = /([0-9]+)[.]{1}([^\\s]+$)/g;
-        var match = regex.exec(string);
-        if (match) {
-          const rowName = {
-            number: match[1],
-            name: match[2]
-          };
-          rowNameList.push(rowName);
-        }
-      }
-    }
-  }
-  rowNameList = _.uniqBy(rowNameList, 'number');
-  rowNameList = await sortObjectInListByNumber(rowNameList);
-
-  return Promise.resolve(rowNameList);
-};
-
-const sortObjectInListByNumber = async function (list) {
-  return Promise.resolve( list.sort(function (a, b) {
-    return a.number > b.number ? 1 : -1;
-  }) );
+  console.log(rowsAttrList);
 };
 
 var compute = {
---
+++ src/assets/findAttrList.js
@@ -0,0 +1,67 @@
+/**
+ * Created by max on 2017/8/22.
+ */
+
+exports.findColsAttr = async function ( firstLine ) {
+
+  var resultList = [];
+
+  for(var i=0; i<firstLine.length; i++) {
+    var myString = firstLine[i];
+    var myRegexp = /([0-9]+)[.]{1}([^\\s]+$)/g;
+    var match = myRegexp.exec(myString);
+    if (match) {
+      var attr = {
+        position: i,
+        number: match[1],
+        name: match[2],
+      };
+      resultList.push(attr);
+    }
+  }
+
+  resultList = await sortObjectInListByNumber(resultList);
+
+  return Promise.resolve(resultList);
+
+};
+
+exports.findRowsAttr = async function ( linesNumberFrom,  lines, colsAttrList ) {
+
+  let shiftsPositionList = [];
+  for (var i=0; i<colsAttrList.length; i++) {
+    if (colsAttrList[i].position !== 0) {
+      shiftsPositionList.push(colsAttrList[i].position);
+    }
+  }
+
+  let rowNameList = [];
+  for (var i=linesNumberFrom; i<lines.length; i++) {
+    for (var j=0; j<shiftsPositionList.length; j++) {
+      const shiftsPosition = shiftsPositionList[j];
+      const personShiftsList = lines[i][shiftsPosition].split(";");
+      for (var k=0; k<personShiftsList.length; k++) {
+        const string = personShiftsList[k];
+        const regex = /([0-9]+)[.]{1}([^\\s]+$)/g;
+        var match = regex.exec(string);
+        if (match) {
+          const rowName = {
+            number: match[1],
+            name: match[2]
+          };
+          rowNameList.push(rowName);
+        }
+      }
+    }
+  }
+  rowNameList = _.uniqBy(rowNameList, 'number');
+  rowNameList = await sortObjectInListByNumber(rowNameList);
+
+  return Promise.resolve(rowNameList);
+};
+
+var sortObjectInListByNumber = async function (list) {
+  return Promise.resolve( list.sort(function (a, b) {
+    return a.number > b.number ? 1 : -1;
+  }) );
+};
